a279b782c04fe0ed9468465e40c88e3ac4b5c356
Kushal Shrestha <kushal-shrestha@hotmail.com>
2024-08-28 15:39:17 +0545

    Added max rate check

--- Assets/Scripts/UpdateCounter.cs
+++ Assets/Scripts/UpdateCounter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class UpdateCounter : MonoBehaviour
 {
@@ -10,6 +11,11 @@ public class UpdateCounter : MonoBehaviour
      private int onGUICount = 0;
      private int pressedCount = 0;
 
+     private int maxGUICount = 0;
+     private int maxUpdateCount = 0;
+     private int maxLateUpdateCount = 0;
+
+
      bool fixedUpdateRun = false;
      bool updateRun = false;
      bool lateUpdateRun = false;
@@ -17,13 +23,31 @@ public class UpdateCounter : MonoBehaviour
      [SerializeField]
      bool logEachFrame = false;
 
+     [SerializeField]
+     Slider rateSlider;
+
      //input 4 TextMeshPro - Text
      public TextMeshPro fixedUpdateText;
      public TextMeshPro UpdateText;
      public TextMeshPro lateUpdateText;
      public TextMeshPro onGUIText;
+
+     public TextMeshPro maxGUIText;
+     public TextMeshPro maxUpdateText;
+     public TextMeshPro maxLateUpdateText;
      void Start()
      {
+          if (rateSlider != null)
+          {
+               // Set an initial value for the slider (optional)
+               rateSlider.value = 50f; // For example, set it to 50 FPS
+                                       // Attach the listener to the slider
+               rateSlider.onValueChanged.AddListener(SetFixedUpdateRate);
+          }
+          else
+          {
+               Debug.LogError("Rate Slider not assigned!");
+          }
           // Start the counting coroutine
           StartCoroutine(CountUpdatesForOneSecond());
      }
@@ -66,6 +90,24 @@ public class UpdateCounter : MonoBehaviour
           Debug.LogWarning("Update count: " + updateCount);
           Debug.LogWarning("LateUpdate count: " + lateUpdateCount);
           Debug.LogWarning("OnGUI count: " + onGUICount);
+
+          //Set Maximums
+          if(maxUpdateCount < updateCount)
+          {
+               maxUpdateCount = updateCount;
+               maxUpdateText.text = maxUpdateCount.ToString();
+          }
+          if (maxLateUpdateCount < lateUpdateCount)
+          {
+               maxLateUpdateCount = lateUpdateCount;
+               maxLateUpdateText.text = maxLateUpdateCount.ToString();
+          }
+          if (maxGUICount < onGUICount)
+          {
+               maxGUICount = onGUICount;
+               maxGUIText.text = maxGUICount.ToString();
+          }
+
           fixedUpdateText.text = fixedUpdateCount.ToString();
           UpdateText.text = updateCount.ToString();
           lateUpdateText.text = lateUpdateCount.ToString();
@@ -82,5 +124,13 @@ public class UpdateCounter : MonoBehaviour
           updateCount = 0;
           lateUpdateCount = 0;
           onGUICount = 0;
+     }
+     public void SetFixedUpdateRate(float rate)
+     {
+          if (rate > 0)
+          {
+               Time.fixedDeltaTime = 1.0f / rate;
+               Debug.Log("FixedUpdate rate set to: " + rate + " calls per second");
+          }
      }
 }
