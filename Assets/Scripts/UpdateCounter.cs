d29efa26c54eb28c2f9ec7250991cc05fa3f46ca
Kushal Shrestha <kushal-shrestha@hotmail.com>
2024-08-30 20:11:20 +0545

    touch latency testing

--- Assets/Scripts/UpdateCounter.cs
+++ Assets/Scripts/UpdateCounter.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using System;
 
 public class UpdateCounter : MonoBehaviour
 {
@@ -14,7 +16,13 @@ public class UpdateCounter : MonoBehaviour
      private int maxGUICount = 0;
      private int maxUpdateCount = 0;
      private int maxLateUpdateCount = 0;
+     float standardDeviation = 0f;
 
+     public float[] deltaTimes;
+     //Calculating S.D
+     private float[] fixedUpdateTimes;  // Array to store time values of each FixedUpdate
+     public float[] lastFrameUpdateTimes;
+     private int maxFixedUpdateSamples = 260;  // Default size of the array
 
      bool fixedUpdateRun = false;
      bool updateRun = false;
@@ -35,28 +43,86 @@ public class UpdateCounter : MonoBehaviour
      public TextMeshPro maxGUIText;
      public TextMeshPro maxUpdateText;
      public TextMeshPro maxLateUpdateText;
+
+     //Touch
+     private TouchInputActions touchInputActions;
+     private Vector2 touchPosition;
+     private bool isTouching;
+
+     private void Awake()
+     {
+          touchInputActions = new TouchInputActions();
+          // Set the Input System to manual update mode
+          InputSystem.settings.updateMode = InputSettings.UpdateMode.ProcessEventsManually;
+     }
      void Start()
      {
+
           if (rateSlider != null)
           {
                // Set an initial value for the slider (optional)
-               rateSlider.value = 50f; // For example, set it to 50 FPS
-                                       // Attach the listener to the slider
+               rateSlider.value = 240f; // For example, set it to 50 FPS
+                                        // Attach the listener to the slider
+               SetFixedUpdateRate(rateSlider.value);
                rateSlider.onValueChanged.AddListener(SetFixedUpdateRate);
           }
           else
           {
                Debug.LogError("Rate Slider not assigned!");
           }
+
+          // Initialize the array for storing FixedUpdate times
+          fixedUpdateTimes = new float[maxFixedUpdateSamples];
+
           // Start the counting coroutine
           StartCoroutine(CountUpdatesForOneSecond());
      }
 
      void FixedUpdate()
      {
+          // Manually process input events right before the physics update
+          InputSystem.Update();
+
+          if (isTouching)
+          {
+               // Handle the touch position here
+               Debug.Log($"Touching at: {touchPosition}");
+          }
+
+          if (fixedUpdateCount < maxFixedUpdateSamples)
+               fixedUpdateTimes[fixedUpdateCount] = Time.realtimeSinceStartup;
+
           fixedUpdateCount++;
           if (logEachFrame)
                Debug.Log("FixedUpdate count: " + fixedUpdateCount + " Frame: " + Time.frameCount);
+     }
+
+     private void OnEnable()
+     {
+          touchInputActions.Enable();
+          touchInputActions.TouchControls.TouchPosition.performed += ctx => OnTouch(ctx);
+          touchInputActions.TouchControls.TouchPosition.canceled += ctx => OnTouchCanceled(ctx);
+     }
+
+     private void OnDisable()
+     {
+          touchInputActions.Disable();
+          touchInputActions.TouchControls.TouchPosition.performed -= ctx => OnTouch(ctx);
+          touchInputActions.TouchControls.TouchPosition.canceled -= ctx => OnTouchCanceled(ctx);
+     }
+
+     private void OnTouch(InputAction.CallbackContext context)
+     {
+          PrintCurrentTimeStamp("Touched on:");
+          isTouching = true;
+          touchPosition = context.ReadValue<Vector2>();
+          Debug.Log($"Touching at: {touchPosition}");
+
+     }
+
+     private void OnTouchCanceled(InputAction.CallbackContext context)
+     {
+          isTouching = false;
      }
 
      void Update()
@@ -83,16 +149,16 @@ public class UpdateCounter : MonoBehaviour
      private IEnumerator CountUpdatesForOneSecond()
      {
           // Wait for 1 second
-          yield return new WaitForSeconds(1f);
+          yield return new WaitForSecondsRealtime(1f);
 
-                    // Output the counts
+          // Output the counts
           Debug.LogWarning("FixedUpdate count: " + fixedUpdateCount);
           Debug.LogWarning("Update count: " + updateCount);
           Debug.LogWarning("LateUpdate count: " + lateUpdateCount);
           Debug.LogWarning("OnGUI count: " + onGUICount);
 
           //Set Maximums
-          if(maxUpdateCount < updateCount)
+          if (maxUpdateCount < updateCount)
           {
                maxUpdateCount = updateCount;
                maxUpdateText.text = maxUpdateCount.ToString();
@@ -102,19 +168,53 @@ public class UpdateCounter : MonoBehaviour
                maxLateUpdateCount = lateUpdateCount;
                maxLateUpdateText.text = maxLateUpdateCount.ToString();
           }
-          if (maxGUICount < onGUICount)
-          {
-               maxGUICount = onGUICount;
-               maxGUIText.text = maxGUICount.ToString();
-          }
+          //if (maxGUICount < onGUICount)
+          //{
+          //     maxGUICount = onGUICount;
+          //     maxGUIText.text = maxGUICount.ToString();
+          //}
 
           fixedUpdateText.text = fixedUpdateCount.ToString();
           UpdateText.text = updateCount.ToString();
           lateUpdateText.text = lateUpdateCount.ToString();
           onGUIText.text = onGUICount.ToString();
 
-          // Optionally, you can reset the counts and start counting again
+
+          // Calculate delta times and standard deviation
+          deltaTimes = new float[maxFixedUpdateSamples - 1];
+          float sum = 0f;
+          int length = 0;
+          for (int i = 1; i < (maxFixedUpdateSamples - 1); i++)
+          {
+               deltaTimes[i - 1] = fixedUpdateTimes[i] - fixedUpdateTimes[i - 1];
+               if (deltaTimes[i - 1] >= 0.0f)
+               {
+                    sum += deltaTimes[i - 1];
+                    length++;
+               }
+          }
+
+          float mean = sum / length;
+
+          Debug.LogWarning("FixedUpdate Sum:" + sum * 1000.0f + "ms, Mean: " + mean * 1000.0f + "ms");
+          float sumOfSquares = 0f;
+          foreach (float delta in deltaTimes)
+          {
+               if (delta >= 0.0f)
+                    sumOfSquares += Mathf.Pow(delta - mean, 2);
+          }
+
+          standardDeviation = Mathf.Sqrt(sumOfSquares / length);
+          Debug.LogWarning("FixedUpdate Standard Deviation: " + standardDeviation);
+
+          maxGUIText.text = standardDeviation.ToString("F5");
+
+          // Reset the array size according to the current slider value
+          maxFixedUpdateSamples = (int)rateSlider.value + 10;
+          lastFrameUpdateTimes = fixedUpdateTimes;
+          fixedUpdateTimes = new float[maxFixedUpdateSamples];
           ResetCounts();
+
           StartCoroutine(CountUpdatesForOneSecond());
      }
 
@@ -132,5 +232,16 @@ public class UpdateCounter : MonoBehaviour
                Time.fixedDeltaTime = 1.0f / rate;
                Debug.Log("FixedUpdate rate set to: " + rate + " calls per second");
           }
+     }
+     private void PrintCurrentTimeStamp(string preText="")
+     {
+          // Get the current system time with millisecond accuracy
+          DateTime now = DateTime.Now;
+
+          // Format the time string with millisecond precision
+          string timeStamp = now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+
+          // Print the time stamp
+          Debug.Log($"{preText} Current Time Stamp: {timeStamp}");
      }
 }
